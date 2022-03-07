cecbab2a30e58bbb443242eb92da9d7a33772545
Ranjan Pandit <59310086+Ranjanunicode@users.noreply.github.com>
2022-03-07 22:46:10 +0530

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,4 +1,4 @@
-
+// Program to check a number is palindrome or not.
 
 #include <iostream>
 using namespace std;
@@ -6,10 +6,11 @@ using namespace std;
 int main() {
     
     int n;
-    cin>>n;
+    cin>>n;// taking input
     
-    int rev=0,temp=n;
+    int rev=0,temp=n; 
     while(temp>0){
+        // If input is not 0 then
         rev = (rev*10)+(temp%10);
         temp=temp/10;
     }

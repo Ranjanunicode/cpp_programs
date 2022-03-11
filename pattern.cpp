2b61235b19f0811baae6902988f1ee18d418b92f
Ranjan Pandit <59310086+Ranjanunicode@users.noreply.github.com>
2022-03-12 01:02:33 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -4,6 +4,7 @@
 
 int main() {
    int i, space, rows, k = 0;
+   // Input no. Of required rows
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
    for (i = 1; i <= rows; ++i, k = 0) {

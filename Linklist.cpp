dc0f5e16bb2ce0f98af9ef8f58cefaadfa46f928
Ranjan Pandit <59310086+Ranjanunicode@users.noreply.github.com>
2022-03-13 23:11:38 +0530

    Create Linklist.cpp

---
+++ Linklist.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<malloc.h>
+void insert_beg();
+void insert_end();
+void display();
+struct node
+{
+    int data;
+    struct node *link;
+    
+};
+struct node *start=NULL;
+
+
+int main() {
+   
+    int a;
+    printf("Enter 1 to insert node at beginning");
+    printf("\nEnter 2 to insert node at end");
+    printf("\nEnter 3 to display");
+    printf("\nEnter 4 to exit");
+   
+    while(1)
+    {
+        printf("\nEnter your choice: ");
+        scanf("%d",&a);
+        switch(a)
+        {
+            case 1 : printf("\nInserting node at beginning");insert_beg();break;
+            case 2 : printf("\nInserting node at the end");insert_end();break;
+            case 3 : printf("Display");display();break;
+            case 4 : exit(0);
+        }
+    }
+    getch();
+    return 0;
+}
+void insert_beg()
+{
+    struct node *temp;
+    temp=(struct node *)malloc(sizeof(struct node));
+    printf("\nEnter a number : ");
+    scanf(" %d",&temp->data);
+    temp->link=NULL;
+    if(start==NULL)
+        start=temp;
+    else 
+    {
+        temp->link=start;
+        start=temp;
+    }  
+}
+void insert_end()
+{
+    struct node *temp,*top;
+    temp=(struct node *)malloc(sizeof(struct node));
+    printf("\nEnter a number : ");
+    scanf(" %d",&temp->data);
+    temp->link=NULL;
+    if(start==NULL)
+        start=temp;
+    else
+    {
+        top=start;
+        while(top!=NULL)
+        {
+            top=top->link;
+        }
+        top->link=temp;
+        temp=top;
+    }  
+}
+void display()
+{
+    struct node *top;
+    if(start==NULL)
+        printf("\nThe link list is empty\n");
+    else   
+    {
+        top = start;
+        while(top!=NULL)
+        {
+            printf("\nElements are : %d",top->data);
+            top=top->link;
+        }
+    }
+}
